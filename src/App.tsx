c2190b2e93bb3225ceb50bdc434a191db2c68f3e
Mauricio Brito <maubot01@gmail.com>
2022-08-27 20:08:12 -0300

    feat: routes added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { PrivateRoutes } from './components';
+import { ROUTES } from './constants';
+// pages
+const Dashboard = React.lazy(() => import('./pages/Dashboard'));
+const Login = React.lazy(() => import('./pages/Login'));
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<PrivateRoutes />}>
+              <Route element={<Dashboard />} path={ROUTES.HOME} />
+            </Route>
+            <Route element={<Login />} path={ROUTES.LOGIN} />
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
+    </>
   );
 }
 
