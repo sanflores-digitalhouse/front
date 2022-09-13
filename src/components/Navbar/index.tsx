27a1cd33514728a1f22e23f0e8b98eaceeb50082
Mauricio Brito <maubot01@gmail.com>
2022-09-13 09:39:11 -0300

    fix: background and position changed

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
 import Avatar from '@mui/material/Avatar';
@@ -19,11 +18,8 @@ export const Navbar = () => {
   const name = 'Mauricio Brito';
   const isAuthenticated = false;
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar
-        className="!tw-bg-background tw-px-10 !tw-text-neutral-gray-100 tw-border-b tw-border-neutral-blue-100 tw-backdrop-blur-lg tw-shadow-none"
-        position="static"
-      >
+    <nav className="tw-w-full tw-fixed tw-z-50">
+      <AppBar className="tw-px-10 tw-bg-transparent !tw-text-neutral-gray-100 tw-border-b tw-border-neutral-blue-100 tw-backdrop-blur tw-shadow-none">
         <Toolbar className="tw-flex tw-px-0 tw-justify-between">
           <Link to={ROUTES.HOME}>
             <Icon className="tw-text-primary" type="digital-house" />
@@ -34,12 +30,15 @@ export const Navbar = () => {
             </Link>
           ) : (
             <div className="tw-flex tw-items-center tw-gap-x-2">
-              <Avatar className="tw-bg-primary tw-rounded-xl" {...stringAvatar(name)} />
+              <Avatar
+                className="tw-bg-primary tw-rounded-xl"
+                {...stringAvatar(name)}
+              />
               Hola, {name}
             </div>
           )}
         </Toolbar>
       </AppBar>
-    </Box>
+    </nav>
   );
 };
