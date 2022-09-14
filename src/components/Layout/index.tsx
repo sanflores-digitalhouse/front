d75c7d9d2b1ae297d7d65535a7634358aeed36f6
Mauricio Brito <maubot01@gmail.com>
2022-09-14 16:44:48 -0300

    isAuthenticated mock added

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -5,12 +5,12 @@ import { Sidebar } from '../Sidebar';
 
 interface NavbarProps {
   children: React.ReactNode;
+  isAuthenticated?: boolean;
 }
 
 const navbarHeight = '4rem';
-const auth = { token: true };
 
-export const Layout = ({ children }: NavbarProps): JSX.Element => {
+export const Layout = ({ children, isAuthenticated = false }: NavbarProps): JSX.Element => {
   return (
     <>
       <div
@@ -19,10 +19,15 @@ export const Layout = ({ children }: NavbarProps): JSX.Element => {
           marginTop: navbarHeight,
         }}
       >
-        <Navbar />
-        {auth.token && <Sidebar />}
+        <Navbar isAuthenticated={isAuthenticated} />
+        {isAuthenticated && <Sidebar />}
 
-        <main className="tw-flex tw-flex-1  tw-flex-wrap tw-overflow-auto">
+        <main
+          className="tw-flex tw-flex-col tw-flex-1  tw-flex-wrap tw-overflow-auto"
+          style={{
+            minHeight: 'calc(100vh - 8rem)',
+          }}
+        >
           {children}
         </main>
       </div>
--- src/components/PrivateRoutes/index.tsx
+++ src/components/PrivateRoutes/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Outlet, Navigate } from 'react-router-dom';
 
-const auth = { token: true };
-export const PrivateRoutes = () => {
-  return auth.token ? <Outlet /> : <Navigate to="/login" />;
+interface PrivateRoutesProps {
+  isAuthenticated: boolean;
+}
+export const PrivateRoutes = ({ isAuthenticated }: PrivateRoutesProps) => {
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
 };
