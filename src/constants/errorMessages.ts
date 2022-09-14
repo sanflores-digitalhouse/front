f8ab77deb28add693e0c46482c1beb51a97a2707
Mauricio Brito <maubot01@gmail.com>
2022-09-14 00:41:39 -0300

    feat: error messages added

--- src/constants/errorMessages.ts
+++ src/constants/errorMessages.ts
@@ -1,9 +1,12 @@
 export const ERROR_MESSAGES = {
   INVALID_EMAIL: 'Correo electrónico inválido',
   INVALID_PASSWORD: 'Contraseña inválida',
+  PASSWORDS_DO_NOT_MATCH: 'Las contraseñas no coinciden',
   INVALID_NAME: 'Nombre inválido',
   INVALID_PHONE: 'Teléfono inválido',
+  INVALID_DNI: 'DNI inválido',
   REQUIRED_FIELD: 'Campo requerido',
   MIN_LENGTH: 'Debe tener al menos 6 caracteres',
   MAX_LENGTH: 'Debe tener menos de 20 caracteres',
+  MIN_LENGHT_NAME: 'Debe tener al menos 2 caracteres',
 };
