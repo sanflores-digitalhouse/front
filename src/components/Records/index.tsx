467c2e7ad18ea57e1a3c16e95218d5a792413f5b
Mauricio Brito <maubot01@gmail.com>
2022-09-18 18:55:21 -0300

    fix: recordsToShow added

--- src/components/Records/index.tsx
+++ src/components/Records/index.tsx
@@ -10,13 +10,22 @@ export interface RecordsProps {
 
 const { activities } = USER;
 
-export const Records = ({ records = activities, maxRecords, initialRecord = 0 }: RecordsProps) => {
+export const Records = ({
+  records = activities,
+  maxRecords,
+  initialRecord = 0,
+}: RecordsProps) => {
+  const recordsToShow = records.slice(initialRecord, maxRecords);
   return (
     <ul className="tw-w-full">
-      {records
-        .slice(initialRecord, maxRecords ? maxRecords : records.length)
-        .map((record: RecordProps, index: number) => (
-          <Record key={`record-${index}`} {...record} />
+      {recordsToShow &&
+        recordsToShow.map((record: RecordProps, index: number) => (
+          <Record
+            key={`record-${index}`}
+            {...record}
+            className={`
+              ${index + 1 === recordsToShow.length && 'tw-border-b'}`}
+          />
         ))}
     </ul>
   );
