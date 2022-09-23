bf81130341c0b116354594ba5d23ef55df6ad62a
Mauricio Brito <maubot01@gmail.com>
2022-09-23 02:06:01 -0300

    feat: user context created

---
+++ src/context/User/index.tsx
@@ -0,0 +1,37 @@
+import React, { createContext, useReducer } from 'react';
+import userReducer from './userReducer';
+// import { userActionTypes } from './types';
+import { User } from '../../common';
+
+export interface UserInfoState {
+  user: User;
+  loading: boolean;
+}
+
+const initialState = {
+  user: {} as User,
+  loading: false,
+};
+
+export const userInfoContext = createContext<{
+  user: User;
+  loading: boolean;
+  dispatch: React.Dispatch<any>;
+}>({
+  ...initialState,
+  dispatch: () => null,
+});
+
+const UserInfoProvider = ({ children }: { children: React.ReactNode }) => {
+  const [state, dispatch] = useReducer(userReducer, initialState);
+
+  return (
+    <userInfoContext.Provider
+      value={{ user: state.user, loading: state.loading, dispatch }}
+    >
+      {children}
+    </userInfoContext.Provider>
+  );
+};
+
+export default UserInfoProvider;
