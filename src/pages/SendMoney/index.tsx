f0cbe59ac89f790913d8c4cb6598134c5d4de3e1
Mauricio Brito <maubot01@gmail.com>
2022-09-29 21:54:41 -0300

    fix: steps edited

--- src/pages/SendMoney/index.tsx
+++ src/pages/SendMoney/index.tsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   CardCustom,
   Icon,
-  RecordProps,
   Records,
   RecordVariant,
+  FormSingle,
 } from '../../components';
-import { ADD, ROUTES } from '../../constants';
-import { Link } from 'react-router-dom';
-import { usePagination } from '../../hooks/usePagination';
+import { ROUTES, STEP } from '../../constants';
+import { Link, useSearchParams, useNavigate } from 'react-router-dom';
 import { USER } from '../../data/user';
-import { Pagination } from '@mui/material';
+import {
+  handleChange,
+  dniValidationConfig,
+  nameValidationConfig,
+  isValueEmpty,
+} from '../../utils';
 
 const { account } = USER;
 const { accounts } = account;
@@ -25,58 +29,116 @@ const userAccounts = accounts.map((account) => {
 });
 
 const SendMoney = () => {
-  const recordsPerPage = 10;
-  const { page, handleChange, numberOfPages, isRecordsGreeterThanOnePage } =
-    usePagination(userAccounts as RecordProps[], recordsPerPage);
+  const [searchParams] = useSearchParams();
+  const step = searchParams.get('step');
+
   return (
     <div className="tw-w-full">
-      <CardCustom
-        className="tw-max-w-5xl"
-        content={
-          <div className="tw-flex tw-justify-between tw-mb-4">
-            <p className="tw-font-bold">Elegí a qué cuenta transferir</p>
-          </div>
-        }
-        actions={
-          <Link
-            to={`${ROUTES.CARDS}?${ADD}`}
-            className="tw-w-full tw-flex tw-items-center tw-justify-between tw-p-4 hover:tw-bg-neutral-gray-500 tw-transition"
-          >
-            <div className="tw-flex tw-items-center tw-gap-x-4">
-              <Icon type="add" />
-              <p>Nueva cuenta</p>
-            </div>
-            <Icon type="arrow-right" />
-          </Link>
-        }
-      />
-      <CardCustom
-        className="tw-max-w-5xl"
-        content={
-          <>
-            <div>
-              <p className="tw-mb-4 tw-font-bold">Últimas cuentas</p>
-            </div>
-            <Records
-              records={userAccounts}
-              initialRecord={page * recordsPerPage - recordsPerPage}
-            />
-          </>
-        }
-        actions={
-          isRecordsGreeterThanOnePage && (
-            <div className="tw-h-12 tw-w-full tw-flex tw-items-center tw-justify-center tw-px-4 tw-mt-4">
-              <Pagination
-                count={numberOfPages}
-                onChange={handleChange}
-                shape="rounded"
-              />
-            </div>
-          )
-        }
-      />
+      {step ? (
+        <SendMoneyForm />
+      ) : (
+        <>
+          <CardCustom
+            className="tw-max-w-5xl"
+            content={
+              <div className="tw-flex tw-justify-between tw-mb-4">
+                <p className="tw-font-bold">Elegí a qué cuenta transferir</p>
+              </div>
+            }
+            actions={
+              <Link
+                to={`${ROUTES.SEND_MONEY}?${STEP}1`}
+                className="tw-w-full tw-flex tw-items-center tw-justify-between tw-p-4 hover:tw-bg-neutral-gray-500 tw-transition"
+              >
+                <div className="tw-flex tw-items-center tw-gap-x-4">
+                  <Icon type="add" />
+                  <p>Nueva cuenta</p>
+                </div>
+                <Icon type="arrow-right" />
+              </Link>
+            }
+          />
+          <CardCustom
+            className="tw-max-w-5xl"
+            content={
+              <>
+                <div>
+                  <p className="tw-mb-4 tw-font-bold">Últimas cuentas</p>
+                </div>
+                <Records records={userAccounts} />
+              </>
+            }
+          />
+        </>
+      )}
     </div>
   );
 };
 
 export default SendMoney;
+
+function SendMoneyForm() {
+  const [formState, setFormState] = useState({
+    alias: '',
+    amount: '',
+  });
+  const [searchParams] = useSearchParams();
+  const step = searchParams.get('step');
+  const navigate = useNavigate();
+
+  const onChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    maxLength?: number
+  ) => handleChange(event, setFormState, maxLength);
+
+  const onSubmit = (step: number) => {
+    navigate(`${ROUTES.SEND_MONEY}?${STEP}${step}`);
+  };
+
+  useEffect(() => {
+    if (step !== '1' && isValueEmpty(formState)) {
+      navigate(`${ROUTES.SEND_MONEY}?${STEP}1`);
+    }
+  }, [step, formState, navigate]);
+
+  return <>{renderStep()}</>;
+
+  function renderStep() {
+    switch (step) {
+      case '1':
+        return (
+          <FormSingle
+            name="alias"
+            title="Agregá una nueva cuenta"
+            label="CVU ó Alias"
+            type="text"
+            actionLabel="Continuar"
+            validation={nameValidationConfig}
+            formState={formState}
+            handleChange={(
+              event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+            ) => onChange(event)}
+            submit={() => onSubmit(2)}
+          />
+        );
+      case '2':
+        return (
+          <FormSingle
+            name="amount"
+            title="¿Cuanto quieres transferir a ?"
+            label="Monto"
+            type="number"
+            actionLabel="Continuar"
+            validation={dniValidationConfig}
+            formState={formState}
+            handleChange={(
+              event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+            ) => onChange(event)}
+            submit={() => onSubmit(3)}
+          />
+        );
+      case '3':
+        return <>3</>;
+    }
+  }
+}
