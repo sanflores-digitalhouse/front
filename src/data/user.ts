5f9b2c311807365a73b5df814b17ef1e66891f13
Mauricio Brito <maubot01@gmail.com>
2022-10-01 18:36:50 -0300

    fix: unnecessary data removed

--- src/data/user.ts
+++ src/data/user.ts
@@ -1,93 +1,12 @@
 export const USER = {
+  id: '12312312312',
   name: 'Mauricio',
   lastName: 'Brito',
   email: 'mau@gmail.com',
   password: '22Paradise**',
   phone: '1234567890',
-  amount: '7000',
-  currency: 'ARS',
-  country: 'Argentina',
-  city: 'Buenos Aires',
+  balance: '7000',
   account: {
-    activities: [
-      {
-        name: 'Aitana',
-        amount: 3500.64,
-        date: '2022-09-17T20:57:38+00:00',
-      },
-      {
-        name: 'Brendys',
-        amount: -1895,
-        date: '2022-09-17T20:57:38+00:00',
-      },
-      {
-        amount: 2000,
-        date: '2022-09-17T20:57:38+00:00',
-      },
-      {
-        name: 'Rodrigo',
-        amount: 2000,
-        date: '2022-09-17T20:57:38+00:00',
-      },
-      {
-        name: 'Rodrigo',
-        amount: 2000,
-        date: '2022-09-17T20:57:38+00:00',
-      },
-      {
-        name: 'Rodrigo',
-        amount: 2000,
-        date: '2022-09-17T20:57:38+00:00',
-      },
-      {
-        name: 'Rodrigo',
-        amount: 2000,
-        date: '2022-09-17T20:57:38+00:00',
-      },
-      {
-        name: 'Rodrigo',
-        amount: 2000,
-        date: '2022-09-17T20:57:38+00:00',
-      },
-      {
-        name: 'Rodrigo',
-        amount: 2000,
-        date: '2022-09-17T20:57:38+00:00',
-      },
-      {
-        name: 'Rodrigo',
-        amount: 2000,
-        date: '2022-09-17T20:57:38+00:00',
-      },
-      {
-        name: 'Mauricio',
-        amount: 2000,
-        date: '2022-09-17T20:57:38+00:00',
-      },
-    ],
-    cards: [
-      {
-        type: 'Debit',
-        number: '1234567890',
-        name: 'Mauricio Brito',
-        expiration: '2022-09-17T20:57:38+00:00',
-        cvv: '123',
-      },
-      {
-        type: 'Debit',
-        number: '1234567890',
-        name: 'Mauricio Brito',
-        expiration: '2022-09-17T20:57:38+00:00',
-        cvv: '123',
-      },
-      {
-        type: 'Debit',
-        number: '1234567890',
-        name: 'Mauricio Brito',
-        expiration: '2022-09-17T20:57:38+00:00',
-        cvv: '123',
-      },
-    ],
     accounts: [
       {
         name: 'John Esteban Doe Cia',
