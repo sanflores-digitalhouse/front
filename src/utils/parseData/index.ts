13ec46fdb78ec089972af631e7c8817f84377b25
Mauricio Brito <maubot01@gmail.com>
2022-10-13 21:22:57 -0300

    fix: unnecessary code removed

--- src/utils/parseData/index.ts
+++ src/utils/parseData/index.ts
@@ -1,43 +1,4 @@
 import { IRecord } from '../../components';
-import { Transaction, Card } from '../../types/';
-
-export const parseActivities = (activities: Transaction[]): Transaction[] => {
-  return activities.map((activity: any) => {
-    const { name, amount, dated, id, type, origin } = activity;
-    return {
-      name,
-      amount,
-      dated,
-      id,
-      type,
-      origin,
-    };
-  });
-};
-
-export const parseActivity = (activity: Transaction): Transaction => {
-  const { name, amount, dated, id, type, origin } = activity;
-  return {
-    name,
-    amount,
-    dated,
-    id,
-    type,
-    origin,
-  };
-};
-
-export const parseCards = (cards: Card[]): Card[] => {
-  return cards.map((card: Card) => {
-    const { name, number, id, type } = card;
-    return {
-      name,
-      number,
-      id,
-      type,
-    };
-  });
-};
 
 export const parseRecordContent = (record: any, variant: any): IRecord => {
   return {
